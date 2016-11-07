0f978de97e00ccf6f0a270a167d5bf0a80735ec1
Richard Zhang <lapislacrima@gmail.com>
2016-11-07 23:05:08 +0000

    fixes comments and deletes commented out code

--- download_avatars.js
+++ download_avatars.js
@@ -1,13 +1,15 @@
 const request = require('request');
-const git_auth = require('./git_auth');
+const gitAuth = require('./git_auth');
 const fs = require('fs');
-const GITHUB_USER = git_auth.user_name;
-const GITHUB_TOKEN = git_auth.token;
+const GITHUB_USER = gitAuth.user_name;
+const GITHUB_TOKEN = gitAuth.token;
 
-console.log('Welcome to the GitHub Avatar Downloader!')
+console.log('Welcome to the GitHub Avatar Downloader!');
 
+// String, String, function -> undefined
+// calls callback function on the request GET of the repo
 function getRepoContributors(repoOwner, repoName, cb){
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
   request.get({
     headers: {
@@ -15,16 +17,14 @@ function getRepoContributors(repoOwner, repoName, cb){
       'Content-Type': 'applicaiton/JSON'
     },
     uri: requestURL
-  }, (error, response, body) => {cb(error, response, body)})
-
-
-  return null;
+  }, (error, response, body) => { cb(error, response, body); });
 }
+
 
 // error, response, body (from request) -> undefined
 // side effects only
 // calls downloadImageByURL on each contributor's login and avatar_url
-function getHandler(error, response, body){
+function getImages(error, response, body){
   if (error){
     throw error;
   }
@@ -32,23 +32,19 @@ function getHandler(error, response, body){
   console.log('Response Content Type: ', response.headers['content-type']);
   // Array of Objects -> Objects
   // Takes the body, which is an array of objects, and retreives/saves login:avatar_url
-  var avatar_urls = {};
+  var avatarUrls = {};
   var parsedBody = JSON.parse(body);
   for (contributor of parsedBody){
     if (contributor.avatar_url){
-      // avatar_urls.push(contributor.avatar_url);
-      // user_names.push(contributor.login);
-      avatar_urls[contributor.login] = contributor.avatar_url;
+      avatarUrls[contributor.login] = contributor.avatar_url;
     }
   }
-  for (name in avatar_urls){
-    if (avatar_urls.hasOwnProperty(name)){
-      downloadImageByURL(avatar_urls[name], './avatars/'+name);
+  for (name in avatarUrls){
+    if (avatarUrls.hasOwnProperty(name)){
+      downloadImageByURL(avatarUrls[name], './avatars/' + name);
     }
   }
-
-  console.log(avatar_urls);
-
+  console.log(avatarUrls);
 }
 
 // url, filePath -> undefined
@@ -57,26 +53,20 @@ function getHandler(error, response, body){
 function downloadImageByURL(url, filePath){
   var filename = filePath.substring(filePath.lastIndexOf('/')+1);
   request.get(url)
-  .on ('error', (err) => {console.log("Error "+err); throw(err);})
-  .on ('response', (response) => {
-    console.log('Downloading image for ' + filename + " ...")
+  .on ('error',(err) => {console.log("Error "+err); throw err;})
+  .on ('response',(response) => {
+    console.log('Downloading image for ' + filename + " ...");
   })
   .pipe(fs.createWriteStream(filePath))
-  .on ('finish', () => {console.log('Downloading complete for ' + filename);})
+  .on ('finish', () => { console.log('Downloading complete for ' + filename); });
 }
 
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//   console.log("Errors:", err);
-//   console.log("Result:", result);
-// });
 
-// getRepoContributors("jquery", "jquery", getHandler);
-
-// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg")
-
+// takes input from the command line
+// recommends input format if input not of length 2
 input = process.argv.slice(2);
 if (input.length !== 2){
-  console.log("Please enter :owner :repo \nThe overall command should look like \nnode download_avatars <owner> <repo>")
+  console.log("Please enter :owner :repo \nThe overall command should look like \nnode download_avatars <owner> <repo>");
 } else {
-  getRepoContributors(input[0], input[1], getHandler);
+  getRepoContributors(input[0], input[1], getImages);
 }
