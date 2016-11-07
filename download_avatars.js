646b947fda92e6918253771ea63a65808f4d9257
Richard Zhang <lapislacrima@gmail.com>
2016-11-07 21:16:06 +0000

    correctly includes trivial header information in request

--- download_avatars.js
+++ download_avatars.js
@@ -7,9 +7,16 @@ console.log('Welcome to the GitHub Avatar Downloader!')
 
 function getRepoContributors(repoOwner, repoName, cb){
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  request.get({
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    },
+    uri: requestURL
+  })
   return true;
 }
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
