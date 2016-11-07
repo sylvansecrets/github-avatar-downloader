7824e7683bb59b605c3c63bbb199e16674a9e6d0
Richard Zhang <lapislacrima@gmail.com>
2016-11-07 20:58:48 +0000

    hard coded test case added

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,13 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!')
+
+function getRepoContributors(repoOwner, repoName, cb){
+  // Not implemented
+  return true;
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
